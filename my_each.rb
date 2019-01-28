661464e5c08f7b9f6835cbec3136d80f24259ba9
John Hayes <johnnyhayes7@gmail.com>
2019-01-28 14:37:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,12 +3,9 @@ def my_each(array)
       while i < array.count
       yield array[i]
       i += 1
-    end
-    else
-      puts "No Block Given!"
-    end
-    array
+    
   end
+  array
 end
 
 my_each(array) {|value| puts value}
